7669390abe8204af8489c5be9eac8d3a568328d5
luizeduardosb <luizeduardo00736@gmail.com>
2025-02-22 17:06:13 -0300

    Iniciando desenvolvimento

---
+++ backend/src/main/java/com/paulino/backend/controller/Hello.java
@@ -0,0 +1,19 @@
+package com.paulino.backend.controller;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/api")
+public class Hello {
+    
+    @GetMapping("/")
+    public String hello() {
+        return "Olá papai " + new Date();
+    }
+
+}
