5a8add2e5cb6486bc57c5eaaf9937ea6a40c7c1d
luizeduardosb <luizeduardo00736@gmail.com>
2025-04-01 15:28:53 -0300

    crud estado

---
+++ backend/src/main/java/com/paulino/backend/repository/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.paulino.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.paulino.backend.entity.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
+    
+}
---
+++ backend/src/main/java/com/paulino/backend/service/EstadoService.java
@@ -0,0 +1,38 @@
+package com.paulino.backend.service;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paulino.backend.entity.Estado;
+import com.paulino.backend.repository.EstadoRepository;
+
+@Service
+public class EstadoService {
+    
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    public List<Estado> buscarTodos() {
+        return estadoRepository.findAll();
+    }
+ 
+    public Estado salvar(Estado estado) {
+        estado.setDataCriação(new Date());
+        Estado estadoNovo = estadoRepository.saveAndFlush(estado);
+        return estadoNovo;
+    }
+
+    public Estado editar(Estado estado) {
+        estado.setDataAtualização(new Date());
+        return estadoRepository.saveAndFlush(estado);
+    }
+
+    public void excluir(Long id) {
+        Estado estado = estadoRepository.findById(id).get();
+        estadoRepository.delete(estado);
+    }
+
+}
